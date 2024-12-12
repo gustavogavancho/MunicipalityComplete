525f6d800cc847e9c5fdce0a62cb4cbaf30958a6
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2024-12-12 01:02:32 -0500

    Municipality Meteo project added

---
+++ MunicipalityMeteo.Api/Program.cs
@@ -0,0 +1,28 @@
+using MunicipalityMeteo.Api.Profiles;
+using MunicipalityMeteo.Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<IWeatherService, WeatherService>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/{municipalityId:int}/meteo", async (int municipalityId, IWeatherService weatherService) =>
+{
+    var municipalityGeo = await weatherService.GetMunicipaliyWeatherDataAsync(municipalityId);
+
+    return Results.Ok(municipalityGeo);
+})
+.WithName("GetMeteorologicInfoByMunicaplityId")
+.WithOpenApi();
+
+app.Run();
