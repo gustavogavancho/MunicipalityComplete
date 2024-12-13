cf0ab9782403f6b39eecbca0263e10b4f853a9c9
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2024-12-12 23:00:37 -0500

    Municipality Orq project added

---
+++ MunicipalityOrq.Api/Dtos/OrchDto.cs
@@ -0,0 +1,10 @@
+﻿using MunicipalityOrq.Api.Models;
+using System.Text.Json.Serialization;
+
+namespace MunicipalityOrq.Api.Dtos;
+
+public class OrchDto
+{
+    [JsonPropertyName("geografia")] public MunicipalityGeoDto GeoData { get; set; }
+    [JsonPropertyName("clima")] public MunicipalityWeatherResponse WeatherData { get; set; }
+}
---
+++ MunicipalityOrq.Api/Program.cs
@@ -0,0 +1,26 @@
+using MunicipalityOrq.Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<IWeatherService, WeatherService>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/{municipalityId:int}/geo/meteo", async (int municipalityId, IWeatherService weatherService) =>
+{
+    var municipalityGeo = await weatherService.GetMunicipaliyDataAsync(municipalityId);
+
+    return Results.Ok(municipalityGeo);
+})
+.WithName("GetGeoAndWeatherInfoByMunicipalityId")
+.WithOpenApi();
+
+app.Run();
